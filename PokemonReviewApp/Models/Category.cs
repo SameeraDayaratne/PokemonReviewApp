3dfad3238dffd1f770f8166abfb0b4a6729c4d70
Dayaratne <Dayaratne@DESKTOP-3J7UOH2>
2024-01-13 12:47:31 +0530

    Created Models

--- PokemonReviewApp/Models/Category.cs
+++ PokemonReviewApp/Models/Category.cs
@@ -1,10 +1,9 @@
 ﻿namespace PokemonReviewApp.Models
 {
-    public class Owner
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public string Gym { get; set; }
     }
 }
--- PokemonReviewApp/Models/Country.cs
+++ PokemonReviewApp/Models/Country.cs
@@ -1,10 +1,9 @@
 ﻿namespace PokemonReviewApp.Models
 {
-    public class Owner
+    public class Country
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public string Gym { get; set; }
     }
 }
